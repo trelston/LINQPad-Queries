8f9704e22b1c6eb6aa7557d653038de99bb2453e
Trelston Moraes <trelstoneurope@outlook.com>
2019-07-03 16:05:55 +0200

    More changes

--- AdventureWorksModel/ReadEFClasses.cs
+++ AdventureWorksModel/ReadEFClasses.cs
@@ -15,20 +15,67 @@ namespace AdventureWorks.ResourceAccess.Tests
             MSBuildLocator.RegisterDefaults();
             MSBuildWorkspace workspace = MSBuildWorkspace.Create();
             Solution solution = workspace
-                .OpenSolutionAsync(@"C:\Users\funct\Downloads\TrelGithub\roslyn-sdk\Samples.sln").Result;
+                .OpenSolutionAsync(@"C:\Users\367463\Source\Repos\roslyn-sdk\Samples.sln").Result;
             Project project = solution.Projects.Single(x => x.Name == "AdventureWorks.ResourceAccess");
             _document = project.Documents.Single(x => x.Name == "HumanResources_Employee.cs");
+            _semanticModel = _document.GetSemanticModelAsync().Result;
         }
 
         private readonly Document _document;
+        public readonly SemanticModel _semanticModel;
 
         private class EmployeeSyntaxWalker : CSharpSyntaxWalker
         {
+            public SemanticModel SemanticModel { get; set; }
+
             public override void VisitPropertyDeclaration(PropertyDeclarationSyntax property)
             {
                 string identifierValueText = property.Identifier.ValueText;
-                object identifierValue = property.Identifier.Value;
+                IPropertySymbol type =  SemanticModel.GetDeclaredSymbol(property);
+
+                foreach (AttributeListSyntax attributeList in property.AttributeLists)
+                {
+                    foreach (AttributeSyntax attribute in attributeList.Attributes)
+                    {
+                        string text = ((IdentifierNameSyntax)attribute.Name).Identifier.ValueText;
+
+                        foreach (AttributeArgumentSyntax item in attribute.ArgumentList.Arguments)
+                        {
+                            new AttributeArgumentWalker
+                            {
+                                SemanticModel = SemanticModel
+                            }.Visit(item);
+                        }
+                    }
+                }
+
                 base.VisitPropertyDeclaration(property);
+            }
+        }
+
+        private class AttributeArgumentWalker : CSharpSyntaxWalker
+        {
+            public SemanticModel SemanticModel { get; set; }
+
+            public override void VisitAttributeArgument(AttributeArgumentSyntax node)
+            {
+                if (node.Expression is MemberAccessExpressionSyntax)
+                {
+                    SymbolInfo text = SemanticModel.GetSymbolInfo(((MemberAccessExpressionSyntax)node.Expression).Expression);
+                }
+
+                if (node.Expression is VariableDeclarationSyntax)
+                {
+
+                }
+
+                if (node.Expression is LiteralExpressionSyntax)
+                {
+                    ISymbol symbol = SemanticModel.GetDeclaredSymbol((AttributeArgumentSyntax)node);
+                    string test = node.Expression.GetFirstToken().ValueText;
+                }
+
+                base.VisitAttributeArgument(node);
             }
         }
 
@@ -36,8 +83,11 @@ namespace AdventureWorks.ResourceAccess.Tests
         public void MyMethod()
         {
             Assert.Equal("HumanResources_Employee.cs", _document.Name);
-
-            new EmployeeSyntaxWalker().Visit(_document.GetSyntaxRootAsync().Result);
+            EmployeeSyntaxWalker walker = new EmployeeSyntaxWalker()
+            {
+                SemanticModel = _semanticModel
+            };
+            walker.Visit(_document.GetSyntaxRootAsync().Result);
         }
     }
 }
